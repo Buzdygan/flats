b9743dc15b6036ffe8bdaf4b7a77ebfddc945e74
Licho59 <lestlalka@gmail.com>
2021-02-13 19:54:30 +0100

    otodom_crawler class has been completed

--- m3/flat_crawler/crawlers/otodom_crawler.py
+++ m3/flat_crawler/crawlers/otodom_crawler.py
@@ -91,33 +91,41 @@ class OtodomCrawler(BaseCrawler):
 
     def _get_url(self, soup: SoupInfo) -> Optional[str]:
         """ Get url of details page of a given offer. """
-        return soup.get('data-url')
+        return soup.base.get('data-url')
 
     def _get_heading(self, soup: SoupInfo) -> Optional[str]:
-        """ Get title of offer. """
-        return soup.find('span', {'class':'offer-item-title'}).text
+        """ Get a title of offer. """
+        return soup.base.find('span', {'class':'offer-item-title'}).text
 
     def _get_district(self, soup: SoupInfo) -> Optional[str]:
-        location = re.search('Warszawa.+', soup.header.p.text)
+        """Get an offer's district name."""
+        location = re.search('Warszawa.+', soup.base.header.p.text)
         return location.group().split()[1]
 
     def _get_price(self, soup: SoupInfo) -> Optional[int]:
-        price = soup.find('li', {'class':'offer-item-price'}).text
+        """Get a price of the offer."""
+        price = soup.base.find('li', {'class':'offer-item-price'}).text
         return int(price.strip().replace('zł','').replace(' ', ''))
 
     def _get_thumbnail_url(self, soup: SoupInfo) -> Optional[str]:
-        """ Url of thumbnail image next to offer title. """
-        return soup.figure.a.get('href')
+        """Get url of thumbnail image next to offer title. """
+        return soup.base.a.span.get('data-src')
 
     def _get_desc(self, soup: SoupInfo) -> Optional[str]:
-        """ Full description of offer. """
-        # available description only on detailed flat page
-        return None
+        """ Return full description of the offer's apartment. """
+        description_tag = soup.detailed.div.find('section', {'role':'region'}).find_all('p')
+        description = ''
+        for elm in description_tag:
+            description += elm.text
+        return description
+
 
     def _get_size_m2(self, soup: SoupInfo) -> Optional[int]:
-        return int(soup.find('li', {'class':'offer-item-area'}).text.split()[0])
+        """Return the size of the offer's apartment."""
+        return int(soup.base.find('li', {'class':'offer-item-area'}).text.split()[0])
 
     def _get_sub_district(self, soup: SoupInfo) -> Optional[str]:
+        """Get name of the subdistrict for the offer if available."""
         sub_district = self._get_district.location.group().split()
         if len(sub_district) > 2:
             return sub_district[2]
@@ -125,27 +133,57 @@ class OtodomCrawler(BaseCrawler):
             return None
 
     def _get_street(self, soup: SoupInfo) -> Optional[str]:
-        # TODO Implement
-        return None
+        """Return the street name -if available - of the object."""
+        heading_search = re.search('ul.', self._get_heading(soup).span()
+        if heading_search:
+            return self._get_heading(soup)[heading_search[0]:]
+
+        map_localisation = soup.detailed.find('a', {'href':'#map'}).text.split()
+        for elm in map_localisation:
+            if 'ul.' in elm:
+                return elm
+            else:
+                return map_localisation[-1]
 
     def _get_img_urls(self, soup: SoupInfo) -> Optional[List[str]]:
         """ Get urls of images on post details page. """
-        #TODO - very deep nested thumbnails 
+        #TODO - very deep level of nesting thumbnail on soup.detailed
         return None
 
     def _get_details_dict(self, soup: SoupInfo) -> Optional[Dict]:
+        """Return all details which are available for the offer.""" 
         if soup.detailed is not None:
             details_dict = {}
-            # TODO
-            #return details_dict
+            details_list = soup.detailed.div.find_all('div', {'role':'region'})
+            for detail in details:
+                detail = detail.text.split(':')
+                details_dict[detail[0]] = detail[1]
+        add_info = self._additional_info()
+        for key,val in add_info.items():
+            details_dict[key] = val
+        return details_dict
+
+    def _additional_info(self) -> Optional(Dict):
+        """Get additional info apart from description part."""
+        new_dict = {}
+        additional_info = soup.detailed.find('div', class_='ad.ad-features.categorized-list').find_all('div')
+            if additional_info is not None:
+                for elm in additional_info:
+                    category = elm.h3.text
+                    cat_values = elm.ul.find_all('li')
+                    new_dict[category] = cat_values
+        return new_dict
 
     def _get_info_dict_json(self, soup: SoupInfo) -> Optional[str]:
         """ Dump to json dictionary any additional information that doesn't fit into models fields.
         """
-        # details_dict: Dict = get_some_details_dict
-        # return json.dumps(details_dict)
-        return None
+        details_dict: Dict = self.get_details_dict(soup=soup)
+        if details_dict is not None:
+            return json.dumps(details_dict)
+        else:
+            return None
 
     def _get_dt_posted(self, soup: SoupInfo) -> Optional[datetime]:
         """ Get datetime of moment when the post was added. """
+        # not available at OTODOM service
         return None
